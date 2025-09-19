eef02699542e06864cfc14597213fe6c065319e1
Gilbert Ngouo <149798152+Ngouo@users.noreply.github.com>
2025-09-19 17:58:51 +0100

    creation de procfile et runtime

--- NgouoSaving/settings.py
+++ NgouoSaving/settings.py
@@ -36,7 +36,8 @@ if ENVIRONMENT == "development":
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ngouo-saving-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://ngouo-saving-production.up.railway.app/']
 
 
 # Application definition
